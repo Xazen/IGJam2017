eccfe984993874add734eb1813a4d645a3ae7d82
Dennis Lieu <dennis.lieu@innogames.com>
2017-07-15 21:12:56 +0200

    Add andy’s scene

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,6 +4,7 @@ using Zenject;
 public class GameController : ITickable
 {
 	public const float RoundDuration = 20f;
+	public const float MercyDuration = 15f;
 	public const int SpawnAmount = 10;
 	public const int SpawnAmountRoundMultiplier = 2;
 	public Vector2 StartingPoint;
@@ -20,6 +21,7 @@ public class GameController : ITickable
 	
 	private float _currentRoundDuration = 0f;
 	private int _round = 1;
+	private bool _isMercyDurationOver = false;
 
 	private bool _gameStarted = false;
 
@@ -45,6 +47,8 @@ public class GameController : ITickable
 	{
 		_gameStarted = false;
 		_round = 0;
+		_isMercyDurationOver = false;
+		_currentRoundDuration = 0;
 	}
 
 	public void Tick()
@@ -54,6 +58,18 @@ public class GameController : ITickable
 		{
 			return;
 		}
+
+		if (!_isMercyDurationOver)
+		{
+			if (_currentRoundDuration < MercyDuration)
+			{
+				_currentRoundDuration += Time.deltaTime;
+				return;
+			}
+
+			_currentRoundDuration = 0;
+			_isMercyDurationOver = true;
+		} 
 		
 		// Check for next round
 		if (_currentRoundDuration < RoundDuration)
