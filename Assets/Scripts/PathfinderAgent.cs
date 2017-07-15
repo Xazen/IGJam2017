23282b7e74b905aac6f3fb76b86a2dacef44276e
Unknown <nilsw.agner@web.de>
2017-07-15 18:26:22 +0200

    added stopping capability to pathfinder

--- Assets/Scripts/PathfinderAgent.cs
+++ Assets/Scripts/PathfinderAgent.cs
@@ -21,10 +21,24 @@ public class PathfinderAgent : MonoBehaviour
 
     private List<Cell> _currentPath;
 
+    private bool _stopMovement;
 
     private Cell _currentCell;
     private Cell _nextCell;
 
+    private bool IsOnStreet(Vector3 world)
+    {
+        try
+        {
+            var posX = Mathf.RoundToInt(world.z);
+            var posY = Mathf.RoundToInt(world.x);
+            return _gridController.Grid[posX, posY].Type == CellType.Street;
+        }
+        catch
+        {
+            return false;
+        }
+    }
 
     private Cell WorldToCell(Vector3 posWorld)
     {
@@ -52,12 +66,18 @@ public class PathfinderAgent : MonoBehaviour
 
     public void CalculatePath(Vector3 targetPosition)
     {
+        Debug.Assert(IsOnStreet(targetPosition), "target must be on street");
+        Debug.Assert(IsOnStreet(transform.position), "agent must be on street");
+
+        _stopMovement = false;
         _targetPosition = targetPosition;
+
         var startCell = WorldToCell(transform.position);
         _targetCell = WorldToCell(_targetPosition);
 
         _currentPath = CalculatePath(startCell, new List<Cell>());
     }
+
 
     public List<Cell> CalculatePath(Cell pos, List<Cell> marked)
     {
@@ -116,14 +136,25 @@ public class PathfinderAgent : MonoBehaviour
         return result;
     }
 
+    public void Stop()
+    {
+        _stopMovement = true;
+    }
+
     private void FollowPath()
     {
-        if (_currentPath == null)
+        if (_currentPath == null || _stopMovement)
             return;
         _nextCell = _currentPath[_currentPath.Count - 2];
         if (Vector2.Distance(WorldToGridPos(transform.position), _nextCell.Position) < 0.4)
         {
             _currentPath.Remove(_currentPath.Last());
+
+            if (_currentPath.Count == 0)
+            {
+                Stop();
+                return;
+            }
         }
         _currentCell = _currentPath.Last();
         if (_currentPath.Last() != _currentCell)
