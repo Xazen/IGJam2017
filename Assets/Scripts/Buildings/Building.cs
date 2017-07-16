9ea2d2a0a3fa619fa8212ba5b800fa144acebac3
Dennis Lieu <dennis.lieu@innogames.com>
2017-07-16 04:12:54 +0200

    Add destruction feedback for building

--- Assets/Scripts/Buildings/Building.cs
+++ Assets/Scripts/Buildings/Building.cs
@@ -1,13 +1,15 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
 using Zenject;
 
 public abstract class Building : MonoBehaviour
 {
-
     public int MaxHealth;
     public Mesh[] BuildingModels;
     public GameObject HealthSlider;
+    public GameObject DamagedParticle;
+    public GameObject DestroyedParticle;
 
     public int BuildingValue;
 
@@ -16,6 +18,7 @@ public abstract class Building : MonoBehaviour
 
     protected GameController _gameController;
     protected bool _destroyed;
+    private GameObject _currentParticle;
 
     void Awake()
     {
@@ -30,12 +33,34 @@ public abstract class Building : MonoBehaviour
 
     public void GetDamage(int damage)
     {
-        if(damage > 0)
+        if (damage > 0)
+        {
             _gameController.RegisterCasualty(CalculateCasualty(damage));
+            if (_health > 0)
+            {
+                StartCoroutine(BlinkDamange());
+            }
+        }
+            
         //if (_slider == null) {
         //    _slider = Instantiate(HealthSlider,transform.position,Quaternion.identity,transform).GetComponentInChildren<Slider>();
         //    _slider.maxValue = MaxHealth;
         //}
+
+        if (_health == MaxHealth &&
+            _health > 0 &&
+            damage > 0 && 
+            _currentParticle == null)
+        {
+            _currentParticle = Instantiate(
+                DamagedParticle, 
+                new Vector3(
+                    transform.position.x, 
+                    transform.position.y + 0.5f, 
+                    transform.position.z), 
+                Quaternion.identity);
+        }
+        
         _health -= damage;
         //_slider.value = _health;
         if (_health <= 0)
@@ -44,9 +69,27 @@ public abstract class Building : MonoBehaviour
         }
     }
 
+    private IEnumerator BlinkDamange()
+    {
+        Color originalColor = GetComponentInChildren<MeshRenderer>().material.GetColor("_EmissionColor");
+        GetComponentInChildren<MeshRenderer>().material.SetColor("_EmissionColor", Color.red);
+        yield return new WaitForEndOfFrame();
+        GetComponentInChildren<MeshRenderer>().material.SetColor("_EmissionColor", originalColor);    
+    }
+
     public virtual void DestroyBuilding()
     {
         _destroyed = true;
+        GetComponentInChildren<MeshRenderer>().material.SetColor("_EmissionColor", Color.red);
+        if (_currentParticle)
+        {
+            Destroy(_currentParticle);
+            _currentParticle = Instantiate(DestroyedParticle, new Vector3(
+                transform.position.x, 
+                transform.position.y + 0.5f, 
+                transform.position.z), 
+                Quaternion.identity);
+        }
     }
 
     private int CalculateCasualty(int dmg)
